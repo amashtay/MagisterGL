2efe3cb40d75ad3d21dbca4a6da471bc2dfe0789
Хохлова Татьяна <t.khokhlova@movavi.com>
2017-06-11 13:29:44 +0700

    init

---
+++ MagisterGL/MagisterGL/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MagisterGL
+//
+//  Created by Хохлова Татьяна on 11.06.17.
+//  Copyright © 2017 Хохлова Татьяна. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder,
+  UIApplicationDelegate
+{
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+  {
+    return true
+  }
+}
